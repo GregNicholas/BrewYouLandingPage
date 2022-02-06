09ae53b5db212321f3a2c48a834362984736d41c
GregNicholas <gregschoenberg@gmail.com>
2022-02-05 21:00:29 -0800

    add product carousel, style responsively, add images

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// Carousel
+// CAROUSEL
+const slides = document.getElementsByClassName("carousel-item");
+const prev = document.getElementById("btn-prev");
+const next = document.getElementById("btn-next");
+const carouselIndex = document.querySelectorAll(".dot");
+const carousel = document.getElementById("carousel");
+const dot = document.getElementById("0");
+
+dot.addEventListener("click", () => console.log("DOT"));
+console.log(dot);
+
+let slidePosition = 2;
+const totalSlides = slides.length;
+
+function hideAllSlides() {
+  for (let i = 0; i < totalSlides; i++) {
+    slides[i].classList.remove("carousel-item-visible");
+    //slides[i].classList.add("carousel-item-hidden");
+    carouselIndex[i].classList.remove("active");
+  }
+}
+
+const nextSlide = () => {
+  hideAllSlides();
+  if (slidePosition < totalSlides - 1) {
+    ++slidePosition;
+  } else {
+    slidePosition = 0;
+  }
+  //slides[slidePosition].classList.remove("carousel-item-hidden");
+  slides[slidePosition].classList.add("carousel-item-visible");
+  carouselIndex[slidePosition].classList.add("active");
+};
+
+const prevSlide = () => {
+  hideAllSlides();
+  if (slidePosition > 0) {
+    --slidePosition;
+  } else {
+    slidePosition = totalSlides - 1;
+  }
+  slides[slidePosition].classList.remove("carousel-item-hidden");
+  slides[slidePosition].classList.add("carousel-item-visible");
+  carouselIndex[slidePosition].classList.add("active");
+};
+
+carouselIndex[0].addEventListener("click", hideAllSlides);
+
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
+
+
+// Toggle Nav Menu
 const toggleMenu = (toggleId, navId) => {
     const toggle_btn = document.getElementById(toggleId),
     nav = document.getElementById(navId)
